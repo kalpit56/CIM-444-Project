fcd2ba7e97b2f70be4ca1a52d77de9ac4ce690d9
Kalpit Mody <kalpitmody@gmail.com>
2021-04-05 02:03:50 -0400

    ground/player/movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float move = Input.GetAxis("Horizontal");
+
+       GetComponent<Rigidbody2D>().velocity = new Vector2(move * speed, GetComponent<Rigidbody2D>().velocity.y);
+    }
+}
