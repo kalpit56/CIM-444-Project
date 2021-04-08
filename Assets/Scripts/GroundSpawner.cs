822a5f0e6fe2cbbf08b79117c3ab7263096837ae
Kalpit Mody <kalpitmody@gmail.com>
2021-04-08 15:07:01 -0400

    updating

---
+++ Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject groundPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(player.transform.position.x > groundPrefab.transform.position.x - 10){
+            GameObject gameObject = Instantiate(groundPrefab, this.transform.position, groundPrefab.transform.rotation);
+            groundPrefab = gameObject;
+        }*/
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision){
+
+        if(collision.gameObject.tag == "GroundSpawn"){
+            Debug.Log("ENTER");
+            GameObject gameObject = Instantiate(groundPrefab, this.transform.position, groundPrefab.transform.rotation);
+        }
+        //groundPrefab = gameObject;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float speed = 5.0f;
+    float speed = 3.0f;
+    Animator anim;
+    //SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        //spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
        float move = Input.GetAxis("Horizontal");
+       //Debug.Log(move);
+
+       if(move > 0){
+           //spriteRenderer.flipX = false;
+           //anim.SetBool("Walking", true);
+           anim.Play("Walk");
+       } 
+       if(move < 0){
+           //anim.SetBool("Walking", false);
+           //spriteRenderer.flipX = true;
+           anim.Play("WalkLeft");
+       } 
+       if(move == 0){
+           //spriteRenderer.flipX = false;
+           anim.Play("Idle");
+       }
 
        GetComponent<Rigidbody2D>().velocity = new Vector2(move * speed, GetComponent<Rigidbody2D>().velocity.y);
     }
