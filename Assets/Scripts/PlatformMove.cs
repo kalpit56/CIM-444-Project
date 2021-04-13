e83c440ce5486b15ebe1396a726689bef27e2d61
Kalpit Mody <kalpitmody@gmail.com>
2021-04-13 01:04:09 -0400

    level design/mechanics update

---
+++ Assets/Scripts/PlatformMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+
+    float speed = 1.5f;
+    float position;
+    bool up;
+    float direction;
+    public float distance;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //platform = this;
+        position = this.transform.position.y;
+        up = true;
+        direction = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.y > position + distance) {
+            direction = -1;
+        } else if(this.transform.position.y < position){
+            direction = 1;
+        }
+        
+        this.transform.position += new Vector3(0, direction * speed, 0) * Time.deltaTime;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     float speed = 3.0f;
+    float jumpForce = 5;
     Animator anim;
+    public GameObject player;
+    public GameObject groundPrefab;
+    float count = 1;
     //SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
@@ -36,6 +40,22 @@ public class PlayerMovement : MonoBehaviour
            anim.Play("Idle");
        }
 
+       if(Input.GetKeyDown(KeyCode.Space)) {
+           GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpForce);
+           anim.Play("Jump");
+       }
+
        GetComponent<Rigidbody2D>().velocity = new Vector2(move * speed, GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision){
+    
+        if(collision.gameObject.tag == "GroundSpawn"){
+            //Debug.Log("ENTER");
+            count++;
+            GameObject gameObject = Instantiate(groundPrefab, new Vector3((count * 11.98f), -1.75f, 0), groundPrefab.transform.rotation);
+        }
+        //groundPrefab = gameObject;
     }
 }
